aaee1e1ece9c0ea0c520ba04561ff3f049f37b23
Jordan Osborn <jordanosborn0@gmail.com>
2018-12-10 07:49:11 +0000

    Modified build script
    Signed-off-by: Jordan Osborn <jordanosborn0@gmail.com>

--- Code/build.py
+++ Code/build.py
@@ -1,6 +1,6 @@
 import subprocess as sp
-from sys import argv, platform, version_info
 from os import chdir
+from sys import argv, platform, version_info
 
 
 def build():
@@ -9,18 +9,29 @@ def build():
 
 
 def install():
-    pass
+    if platform == "darwin":
+        pass
+    elif platform == "unix":
+        pass
+    else:
+        pass
+
 
 def run():
-    pass
+    sp.call(["./Code"])
 
 
 if __name__ == "__main__":
-    if version_info.major != 3: raise RuntimeError("Please run build script using python3.")
+    if version_info.major != 3 or version_info.minor < 6:
+        raise RuntimeError("Please run build script using python3.6 or greater")
 
     directory = "/".join(__file__.split("/")[:-1])
     chdir(directory)
-    if "--build" in argv: build()
-    elif "--install" in argv: build(); install()
-    elif "--run" in argv: build(); run()
-
+    if "--build" in argv:
+        build()
+    elif "--install" in argv:
+        build();
+        install()
+    elif "--run" in argv:
+        build();
+        run()
