454b7b48756a876ce331ec725d67b74654217783
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-23 16:40:59 +0000

    now runs on mac need to compile arrayfire v3.6 from source

---
+++ fft/fft.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+import os
+import subprocess as sp
+import sys
+
+directory = os.path.dirname(os.path.realpath(__file__))
+
+
+def build(profile):
+    if profile == "":
+        sp.call(["cargo", "build", "--package", "fft", "--bin", "fft"])
+    else:
+        sp.call(["cargo", "build", f"--{profile}", "--package", "fft", "--bin", "fft"])
+
+
+def run(profile):
+    build(profile)
+    if profile == "":
+        sp.call([directory + f"/target/debug/fft"])
+    else:
+        sp.call([directory + f"/target/{profile}/fft"])
+
+
+if __name__ == "__main__":
+    os.chdir(directory)
+    dispatch = {"build": build, "run": run}
+    if len(sys.argv) != 2 and sys.argv[1] not in dispatch.keys():
+        print("Incorrect arguments given!")
+        exit(-1)
+    profile = "release" if len(sys.argv) == 3 and sys.argv[2].lower() == "release" else ""
+    dispatch_func = dispatch[sys.argv[1]]
+    dispatch_func(profile)
--- fft/src/main.rs
+++ fft/src/main.rs
@@ -2,15 +2,15 @@ use arrayfire as af;
 use arrayfire::print_gen;
 use flame as fl;
 
-fn main() { 
+fn main() {
     fl::span_of("name", || {
-        let _guard = fl::start_guard("a");
-        let num_rows: u64 = 50;
-        let num_cols: u64 = 30;
+        af::set_backend(af::Backend::OPENCL);
+        let num_rows: u64 = 500;
+        let num_cols: u64 = 300;
         let dims = af::Dim4::new(&[num_rows, num_cols, 1, 1]);
         let engine = af::RandomEngine::new(af::RandomEngineType::MERSENNE_GP11213, None);
-        let a: af::Array<f64> = af::random_uniform(dims, &engine);
-        af::af_print!("Create a 5-by-3 matrix of random floats on the GPU", a);
+        let a: af::Array<f32> = af::random_uniform(dims, &engine);
+        //af::af_print!("Create a 5-by-3 matrix of random floats on the GPU", a);
     });
     let spans = fl::spans();
     println!("{:?}", spans);
