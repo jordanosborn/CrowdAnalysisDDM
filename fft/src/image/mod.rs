45f47e4601cdfb2626bb16bb5961dce090a9b96f
Jordan Osborn <jordanosborn@users.noreply.github.com>
2019-01-24 16:52:03 +0000

    added tests

--- fft/src/image/mod.rs
+++ fft/src/image/mod.rs
@@ -9,3 +9,14 @@ pub fn add_safe(x: i32, y: i32) -> i32 {
     }
     z
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add_safe(1, 2), 3);
+    }
+}
