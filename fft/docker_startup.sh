128199759127b1ce0fa11b844ec0c1baa9623e28
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-25 22:51:32 +0100

    added script

--- fft/docker_startup.sh
+++ fft/docker_startup.sh
@@ -4,8 +4,8 @@ cd $HOME
 source $HOME/.cargo/env
 rustup self update
 rustup update
-echo -e "export AF_PATH='/opt/arrayfire'" >> $HOME/.bashrc
-echo -e "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:$AF_PATH/lib64'" >> $HOME/.bashrc
+echo -e "export AF_PATH='/usr/local'" >> $HOME/.bashrc
+echo -e "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:$AF_PATH/lib'" >> $HOME/.bashrc
 source $HOME/.bashrc
 
 
