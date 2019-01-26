ae0548f77b7867bbb7743cb9b9c37cb95e7859d6
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-26 18:23:58 +0100

    add

--- fft/docker_startup.sh
+++ fft/docker_startup.sh
@@ -7,6 +7,7 @@ rustup update
 echo -e "export AF_PATH='/usr/local'" >> $HOME/.bashrc
 echo -e "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:$AF_PATH/lib'" >> $HOME/.bashrc
 source $HOME/.bashrc
+cd MastersProject
 git pull
 
 bash
