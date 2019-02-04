6b21e9da6059dbc451c5350cb68affdcdb3a2105
Jordan Osborn <jordanosborn0@gmail.com>
2019-02-04 15:28:06 +0000

    fixed some problems reported by vscode

--- code/lib/include/vidstream.hpp
+++ code/lib/include/vidstream.hpp
@@ -13,44 +13,44 @@ static std::vector<cv::VideoCapture> streams;
 
 using Pixel = cv::Point3_<uint8_t>;
 
-extern "C" void *get_frame(size_t);
+extern "C" {
+    void *get_frame(size_t);
 
-extern "C" size_t start_capture(const char*);
+    size_t start_capture(const char*);
 
-extern "C" size_t start_camera_capture();
+    size_t start_camera_capture();
 
-extern "C" void show(const cv::Mat&);
+    void show(const cv::Mat&);
 
-extern "C" void write(const char*, const cv::Mat*);
+    void write(const char*, const cv::Mat*);
 
-extern "C" void show_next(size_t stream_id);
+    void show_next(size_t stream_id);
 
-extern "C" void *mat_new();
+    void *mat_new();
 
-extern "C" int mat_rows(const cv::Mat*);
+    int mat_rows(const cv::Mat*);
 
-extern "C" int mat_cols(const cv::Mat*);
+    int mat_cols(const cv::Mat*);
 
-extern "C" int mat_depth(const cv::Mat*);
+    int mat_depth(const cv::Mat*);
 
-extern "C" int mat_channels(const cv::Mat*);
+    int mat_channels(const cv::Mat*);
 
-extern "C" const uint8_t *mat_data(const cv::Mat*);
+    const uint8_t *mat_data(const cv::Mat*);
 
-extern "C" void mat_drop(cv::Mat*);
+    void mat_drop(cv::Mat*);
 
+    int mat_type(const cv::Mat* const);
 
-extern "C" int mat_type(const cv::Mat* const);
+    const uint8_t* mat_data(const cv::Mat* const);
 
-extern "C" const uint8_t* mat_data(const cv::Mat* const);
+    size_t mat_total(const cv::Mat* const);
 
-extern "C" size_t mat_total(const cv::Mat* const);
+    size_t mat_elem_size(const cv::Mat* const);
 
-extern "C" size_t mat_elem_size(const cv::Mat* const);
+    size_t mat_elem_size1(const cv::Mat* const);
 
-extern "C" size_t mat_elem_size1(const cv::Mat* const);
+    size_t mat_step1(const cv::Mat* const, int);
 
-extern "C" size_t mat_step1(const cv::Mat* const, int);
-
-
+}
 #endif
