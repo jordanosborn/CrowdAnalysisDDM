b25af555755766a116486466ef68fd23d85b8170
Jordan Osborn <jordanosborn0@gmail.com>
2019-03-05 13:30:03 +0000

    fixed encoding of output videos

--- code/stitcher.py
+++ code/stitcher.py
@@ -10,12 +10,15 @@ import cv2
 def save(data):
     dirpath, _, files = data
     files = list(sorted(filter(lambda s: s.find('.jpg') != -1 or s.find('.png') != -1, files)))
-    name = f"output/{dirpath.split('/')[-1]}.avi"
-    if name != ".avi" and len(files) != 0:
+    name = f"output/{dirpath.split('/')[-1]}.m4v"
+    if name != ".m4v" and len(files) != 0:
         frame = cv2.imread(os.path.join(os.getcwd(), dirpath, files[0]))
         height, width, _ = frame.shape
-        video = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*"DIVX"), 2.0, (width, height))
-        for f in files: video.write(cv2.imread(os.path.join(dirpath, f)))
+        video = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*"H264"), 2.0, (width, height))
+        for i, f in enumerate(files):
+            img = cv2.imread(os.path.join(dirpath, f))
+            cv2.putText(img, f"dt={i+1}", (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            video.write(img)
         cv2.destroyAllWindows()
         video.release()
     return name
