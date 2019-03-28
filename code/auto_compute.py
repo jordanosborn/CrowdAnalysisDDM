4284c68dfe90b53a5af2fb45f130bd9334cd2696
Jordan Osborn <jordanosborn0@gmail.com>
2019-03-28 13:28:29 +0000

    modified script

--- code/auto_compute.py
+++ code/auto_compute.py
@@ -33,12 +33,18 @@ def run(command: str, video: str, capacity: int, radial_width: int):
 
 
 def upload():
+<<<<<<< HEAD
     sp.call(["git", "add", "."])
     sp.call(["git", "commit", "-m", "\"added more data\""])
+=======
+    sp.call(["git", "add", "'*.csv'"])
+    sp.call([["git", "commit", "-m", "'added more data'"]])
+>>>>>>> modified script
     sp.call(["git", "pull", "--rebase"])
     sp.call(["git", "push"])
 
 
+<<<<<<< HEAD
 def contains_any(string: str, to_check: List[str]) -> bool:
     return any(map(lambda x: string.find(x) != -1, to_check))
 
@@ -77,6 +83,22 @@ if __name__ == "__main__":
             ".csv") != -1 and x.find("transposed") == -1, incomplete_filter(files, "./results-transposed")))
         retranspose(files_filtered)
         upload()
+=======
+if __name__ == "__main__":
+    if len(sys.argv) == 5 and sys.argv[1] in ["video-multi-ddm", "video-ddm"]:
+        files = []
+        capacity, radial_width = int(sys.argv[3]), int(sys.argv[4])
+        for (dirpath, dirnames, filenames) in os.walk(sys.argv[2]):
+            files.extend(map(lambda s: f"./{dirpath}{s}", filenames))
+
+        for index, video in enumerate(files):
+            run(sys.argv[1], video, capacity, radial_width)
+            if index % 5 == 0 and index != 0:
+                send_message(
+                    secrets["twilio"],
+                    f"Have completed approximately {index * 100 / len(files)}%.")
+                upload()
+>>>>>>> modified script
     else:
         if len(sys.argv) == 3 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
             sys.argv = sys.argv + [80, 1]
