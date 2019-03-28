8d136d03e7bfeff414ad89dbc6b1beefc8356bc3
Jordan Osborn <jordanosborn0@gmail.com>
2019-03-28 13:34:09 +0000

    need to complete filter to cache already completed

--- code/auto_compute.py
+++ code/auto_compute.py
@@ -33,18 +33,12 @@ def run(command: str, video: str, capacity: int, radial_width: int):
 
 
 def upload():
-<<<<<<< HEAD
     sp.call(["git", "add", "."])
     sp.call(["git", "commit", "-m", "\"added more data\""])
-=======
-    sp.call(["git", "add", "'*.csv'"])
-    sp.call([["git", "commit", "-m", "'added more data'"]])
->>>>>>> modified script
     sp.call(["git", "pull", "--rebase"])
     sp.call(["git", "push"])
 
 
-<<<<<<< HEAD
 def contains_any(string: str, to_check: List[str]) -> bool:
     return any(map(lambda x: string.find(x) != -1, to_check))
 
@@ -54,8 +48,6 @@ def incomplete_filter(files: List[str], directory: str) -> Iterable[str]:
     for (_, dirnames, _) in os.walk(directory):
         completed_videos.extend(dirnames)
     return filter(lambda x: not contains_any(x, completed_videos), files)
-
-# TODO
 
 
 def filter_non_videos(files: Union[Iterable[str], List[str]]) -> Iterable[str]:
@@ -75,48 +67,23 @@ def retranspose(files: List[str]):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1] == "retranspose":
-        files = []
-        for (dirpath, dirnames, filenames) in os.walk("./results"):
-            files.extend(map(lambda s: f"./{dirpath}/{s}", filenames))
-        files_filtered = list(filter(lambda x: x.find(
-            ".csv") != -1 and x.find("transposed") == -1, incomplete_filter(files, "./results-transposed")))
-        retranspose(files_filtered)
-        upload()
-=======
-if __name__ == "__main__":
-    if len(sys.argv) == 5 and sys.argv[1] in ["video-multi-ddm", "video-ddm"]:
+    if len(sys.argv) == 3 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
+        sys.argv = sys.argv + [80, 1]
+    if len(sys.argv) == 5 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
         files = []
         capacity, radial_width = int(sys.argv[3]), int(sys.argv[4])
         for (dirpath, dirnames, filenames) in os.walk(sys.argv[2]):
             files.extend(map(lambda s: f"./{dirpath}{s}", filenames))
-
-        for index, video in enumerate(files):
+        files_filtered = incomplete_filter(files, "./results")
+        files_filtered = list(filter_non_videos(files_filtered))
+        print(f"{len(files_filtered)}/{len(files)} left to analyse.")
+        for index, video in enumerate(files_filtered):
             run(sys.argv[1], video, capacity, radial_width)
-            if index % 5 == 0 and index != 0:
+            if index % 3 == 0 and index != 0:
                 send_message(
                     secrets["twilio"],
-                    f"Have completed approximately {index * 100 / len(files)}%.")
+                    f"Have completed approximately {(index + len(files) -len(files_filtered)) * 100 / len(files)}%.")
                 upload()
->>>>>>> modified script
     else:
-        if len(sys.argv) == 3 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
-            sys.argv = sys.argv + [80, 1]
-        if len(sys.argv) == 5 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
-            files = []
-            capacity, radial_width = int(sys.argv[3]), int(sys.argv[4])
-            for (dirpath, dirnames, filenames) in os.walk(sys.argv[2]):
-                files.extend(map(lambda s: f"./{dirpath}{s}", filenames))
-            files_filtered = incomplete_filter(files, "./results")
-            files_filtered = list(filter_non_videos(files_filtered))
-            print(f"{len(files_filtered)}/{len(files)} left to analyse.")
-            for index, video in enumerate(files_filtered):
-                run(sys.argv[1], video, capacity, radial_width)
-                if index % 3 == 0 and index != 0:
-                    send_message(
-                        secrets["twilio"],
-                        f"Have completed approximately {(index + len(files) -len(files_filtered)) * 100 / len(files)}%.")
-                    upload()
-        else:
-            print(
-                f"Arguments supplied are incorrect (_, directory, capacity, radial_width) - {sys.argv}")
+        print(
+            f"Arguments supplied are incorrect (_, directory, capacity, radial_width) - {sys.argv}")
