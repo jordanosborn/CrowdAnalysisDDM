c29cdada197116f380be6391fde61c4c4ac03533
Jordan Osborn <jordan@osborn.dev>
2019-04-01 22:41:46 +0100

    fixed linter errors

--- code/auto_compute.py
+++ code/auto_compute.py
@@ -3,7 +3,6 @@ import json
 import sys
 import subprocess as sp  # nosec
 import os
-import multiprocessing
 from twilio.rest import Client
 from typing import Union, Any, List, Iterable
 
@@ -18,23 +17,21 @@ def send_message(secrets: Any, body: str):
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        body=body,
-        from_=f'{secrets["twilio_number"]}',
-        to=f'{secrets["phone_number"]}'
+        body=body, from_=f'{secrets["twilio_number"]}', to=f'{secrets["phone_number"]}'
     )
-    print(
-        f'Sent message to {secrets["phone_number"]} message_ID = {message.sid}')
+    print(f'Sent message to {secrets["phone_number"]} message_ID = {message.sid}')
 
 
-def run(command: str, video: str, capacity: int, radial_width: int):
+def run(command: str, video: str, capacity: str, radial_width: str):
     print(video)
-    sp.call(["cargo", "run", "--release", command,
-             str(capacity), str(radial_width), video])
+    sp.call(
+        ["cargo", "run", "--release", command, str(capacity), str(radial_width), video]
+    )
 
 
 def upload():
     sp.call(["git", "add", "."])
-    sp.call(["git", "commit", "-m", "\"added more data\""])
+    sp.call(["git", "commit", "-m", '"added more data"'])
     sp.call(["git", "pull", "--rebase"])
     sp.call(["git", "push"])
 
@@ -57,21 +54,36 @@ def filter_non_videos(files: Union[Iterable[str], List[str]]) -> Iterable[str]:
 
 def retranspose(files: List[str]):
     for i, f in enumerate(files):
-        sp.call(["cargo", "run", "--release",
-                 "retranspose", f.replace("./", ""), "output.csv"])
-        file_path = f.replace("./", "").replace(
-            "results", "results-transposed")
+        sp.call(
+            [
+                "cargo",
+                "run",
+                "--release",
+                "retranspose",
+                f.replace("./", ""),
+                "output.csv",
+            ]
+        )
+        file_path = f.replace("./", "").replace("results", "results-transposed")
         os.mkdir("/".join(file_path.split("/")[0:-1]))
         sp.call(["mv", "output.csv", file_path])
         print(f"Completed {(i+1) * 100 / len(files)}%.")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
-        sys.argv = sys.argv + [80, 1]
-    if len(sys.argv) == 5 and sys.argv[1] in ["video-multi-ddm", "video-ddm"] and os.path.isdir(sys.argv[2]):
-        files = []
-        capacity, radial_width = int(sys.argv[3]), int(sys.argv[4])
+    if (
+        len(sys.argv) == 3
+        and sys.argv[1] in ["video-multi-ddm", "video-ddm"]
+        and os.path.isdir(sys.argv[2])
+    ):
+        sys.argv = sys.argv + ["80", "1"]
+    if (
+        len(sys.argv) == 5
+        and sys.argv[1] in ["video-multi-ddm", "video-ddm"]
+        and os.path.isdir(sys.argv[2])
+    ):
+        files: List[str] = []
+        capacity, radial_width = sys.argv[3], sys.argv[4]
         for (dirpath, dirnames, filenames) in os.walk(sys.argv[2]):
             files.extend(map(lambda s: f"./{dirpath}{s}", filenames))
         files_filtered = incomplete_filter(files, "./results")
@@ -79,11 +91,13 @@ if __name__ == "__main__":
         print(f"{len(files_filtered)}/{len(files)} left to analyse.")
         for index, video in enumerate(files_filtered):
             run(sys.argv[1], video, capacity, radial_width)
-            if index % 3 == 0 and index != 0:
+            if index % 5 == 0 and index != 0:
                 send_message(
                     secrets["twilio"],
-                    f"Have completed approximately {round((index + len(files) -len(files_filtered)) * 100 / len(files), 2)}%.")
+                    f"Have completed approximately {round((index + len(files) - len(files_filtered)) * 100 / len(files), 2)}%.",
+                )
                 upload()
     else:
         print(
-            f"Arguments supplied are incorrect (_, directory, capacity, radial_width) - {sys.argv}")
+            f"Arguments supplied are incorrect (_, directory, capacity, radial_width) - {sys.argv}"
+        )
