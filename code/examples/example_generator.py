10f903b8d6f200970f49eafe2241d62b738dc069
Jordan Osborn <jordanosborn0@gmail.com>
2019-04-07 10:10:25 +0100

    working

--- code/examples/example_generator.py
+++ code/examples/example_generator.py
@@ -1,17 +1,11 @@
 import cv2
 import numpy as np
 
-WHITE = (255, 255, 255)
-
 
 def brownian(N, t, mu, sigma, dt=0.1):
     return zip(
-        np.sqrt(dt)
-        * sigma
-        * np.random.normal(mu * t, sigma * (t ** 2 if mu != 0 else 1), N),
-        np.sqrt(dt)
-        * sigma
-        * np.random.normal(mu * t, sigma * (t ** 2 if mu != 0 else 1), N),
+        np.sqrt(dt) * sigma * np.random.normal(mu, sigma, N),
+        np.sqrt(dt) * sigma * np.random.normal(mu, sigma, N),
     )
 
 
@@ -21,8 +15,8 @@ class particle:
         self.y = np.random.uniform(dL, L - dL)
 
     def update(self, L, dx, dy):
-        self.x = (self.x + dx) % L
-        self.y = (self.y + dy) % L
+        self.x = self.x + dx
+        self.y = self.y + dy
 
 
 if __name__ == "__main__":
@@ -31,23 +25,24 @@ if __name__ == "__main__":
     N, L, dL = 1000, 800, 10
     mu, sigma = 0, 3
     video = cv2.VideoWriter("out.avi", cv2.VideoWriter_fourcc(*"H264"), fps, (L, L))
-    particles = map(lambda _: particle(L, dL), range(N))
+    particles = list(map(lambda _: particle(L, dL), range(N)))
     for _ in range(frames):
         img = np.zeros((L, L, 3), np.uint8)
-        delta = brownian(N, t, mu, sigma, dt)
+        delta = list(brownian(N, t, mu, sigma, dt))
         for p, d in zip(particles, delta):
-            px, py = int(p.x) % L, int(p.y) % L
+            px, py = int(p.x), int(p.y)
             px_p1, py_p1 = (px + 1) % L, (py + 1) % L
             px_m1, py_m1 = (px - 1) % L, (py - 1) % L
-            img[px][py] = WHITE
-            img[px_p1][py] = WHITE
-            img[px][py_p1] = WHITE
-            img[px_p1][py_p1] = WHITE
-            img[px_m1][py] = WHITE
-            img[px][py_m1] = WHITE
-            img[px_m1][py_m1] = WHITE
-            img[px_p1][py_m1] = WHITE
-            img[px_m1][py_p1] = WHITE
+            px, py = px % L, py % L
+            img[px][py] = (255, 255, 255)
+            img[px_p1][py] = (255, 255, 255)
+            img[px][py_p1] = (255, 255, 255)
+            img[px_p1][py_p1] = (255, 255, 255)
+            img[px_m1][py] = (255, 255, 255)
+            img[px][py_m1] = (255, 255, 255)
+            img[px_m1][py_m1] = (255, 255, 255)
+            img[px_p1][py_m1] = (255, 255, 255)
+            img[px_m1][py_p1] = (255, 255, 255)
             p.update(L, *d)
         t += dt
         video.write(img)
