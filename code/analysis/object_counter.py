ad7272daf62b42c7237a30854b15b046690e5bb4
Jordan Osborn <jordanosborn0@gmail.com>
2019-04-08 14:43:08 +0100

    add

--- code/analysis/object_counter.py
+++ code/analysis/object_counter.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
-#https://pjreddie.com/darknet/yolo/
+# https://pjreddie.com/darknet/yolo/
 import subprocess
 
 import cv2
 import sys
+import os
+
+darknet_commands = [
+    "./darknet/darknet",
+    "detect",
+    "./darknet/cfg/yolov3.cfg",
+    "./darknet/yolov3.weights",
+]
+
 
 if __name__ == "__main__":
-    cv2.
+    video = sys.argv[1]
+    if os.path.isfile(video):
+        extension = os.path.splitext(os.path.basename(video))[-1]
+        name = os.path.basename(video).replace(extension, "")
+        if os.path.isdir("temp"):
+            os.removedirs("temp")
+        os.mkdir("temp")
+        
