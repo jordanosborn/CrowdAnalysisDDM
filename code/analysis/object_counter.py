2b841782baf3aa5b148fadf2a02dfc5d84954706
Jordan Osborn <jordanosborn0@gmail.com>
2019-04-08 15:42:58 +0100

    add

--- code/analysis/object_counter.py
+++ code/analysis/object_counter.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 # https://pjreddie.com/darknet/yolo/
-import subprocess
+import subprocess as sp  # nosec
+import shutil
+import natsort
+import re
 
 import cv2
 import sys
 import os
 
-darknet_commands = [
-    "./darknet/darknet",
-    "detect",
-    "./darknet/cfg/yolov3.cfg",
-    "./darknet/yolov3.weights",
-]
+darknet_commands = ["./darknet", "detect", "./cfg/yolov3.cfg", "./yolov3.weights"]
+
+
+def save_images(video: str, directory: str):
+    video_capture = cv2.VideoCapture(video)
+    success, image = video_capture.read()
+    count = 0
+    while success:
+        cv2.imwrite(f"{directory}/{count}.jpg", image)
+        success, image = video_capture.read()
+        count += 1
 
 
 if __name__ == "__main__":
@@ -19,7 +27,25 @@ if __name__ == "__main__":
     if os.path.isfile(video):
         extension = os.path.splitext(os.path.basename(video))[-1]
         name = os.path.basename(video).replace(extension, "")
+        video = "../" + video
+        os.chdir("darknet")
         if os.path.isdir("temp"):
-            os.removedirs("temp")
+            shutil.rmtree("temp")
         os.mkdir("temp")
-        
+        save_images(video, "temp")
+        img_files = [filenames for (dirpath, dirnames, filenames) in os.walk("temp")][0]
+        img_files = natsort.natsorted(
+            [f"./temp/{f}" for f in img_files if f != "0.jpg"],
+            alg=natsort.ns.IGNORECASE,
+        )
+        count = 0
+        person_regex = re.compile(r"person: (\d{1,3})%")
+        for i, f in enumerate(img_files):
+            proc = sp.Popen(darknet_commands + [f], stdout=sp.PIPE)
+            count += len(person_regex.findall(str(proc.communicate()[0])))
+            if i % 10 == 0:
+                print(f"{100 * i / len(img_files)}% complete.")
+        print(f"Average number of people in each frame ~ {count / len(img_files)}")
+        if os.path.isdir("temp"):
+            shutil.rmtree("temp")
+        os.chdir("..")
