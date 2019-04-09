2ce633596587bf128889cadea8c3af1b6b34362d
Jordan Osborn <jordanosborn0@gmail.com>
2019-04-09 17:30:16 +0100

    modified

--- code/analysis/plotter.py
+++ code/analysis/plotter.py
@@ -7,41 +7,45 @@ import sys
 if __name__ == "__main__":
     conn = sqlite3.connect("crowd.sqlite")
     if len(sys.argv) == 3 and sys.argv[1] == "search" and sys.argv[2] == "video":
-        while True:
-            with conn:
-                tables = list(
-                    filter(
-                        lambda s: s.find(sys.argv[2]) != -1,
-                        map(
-                            lambda x: x[0],
-                            conn.execute(
-                                "select name from sqlite_master where type = 'table'"
-                            ).fetchall(),
-                        ),
-                    )
-                )
-            print("Found: ", "\n".join([str((i, t)) for i, t in enumerate(tables)]))
-            table_id = int(input("Which would you like: "))  # nosec
-            q = float(input("Which q would you like [1.5, 499.5, 1.0]? "))  # nosec
-            if 0 <= table_id < len(tables) and 1.5 <= q <= 499.5 and int(q) == q - 0.5:
-                table = tables[table_id]
-                print(f"You have selected{table} at q={q}")
-                with conn:
-                    data = conn.execute(
-                        f"select * from {table} where q={q}"
-                    ).fetchone()[1:]
-                tau = [i + 1 for i in range(len(data))]
 
-                plt.title(
-                    f"Plot of Intensity delta ({table.replace('video_', '')}) for q={q} vs frame difference tau"
+        with conn:
+            tables = list(
+                filter(
+                    lambda s: s.find(sys.argv[2]) != -1,
+                    map(
+                        lambda x: x[0],
+                        conn.execute(
+                            "select name from sqlite_master where type = 'table'"
+                        ).fetchall(),
+                    ),
                 )
-                plt.ylabel(f"I(q={q}, tau)")
-                plt.xlabel("tau")
-                plt.plot(tau, data)
-                plt.show()
-            elif table_id == -1:
-                break
-            else:
-                print("Invalid table selected!")
+            )
+        print("Found: ", "\n".join([str((i, t)) for i, t in enumerate(tables)]))
+        table_id = int(input("Which would you like: "))  # nosec
+        if 0 <= table_id < len(tables):
+            while True:
+                q = float(input("Which q would you like [1.5, 499.5, 1.0]? "))  # nosec
+                if 1.5 <= q <= 499.5 and int(q) == q - 0.5:
+                    table = tables[table_id]
+                    print(f"You have selected {table} at q={q}")
+                    with conn:
+                        data = conn.execute(
+                            f"select * from {table} where q={q}"
+                        ).fetchone()[1:]
+                    tau = [i + 1 for i in range(len(data))]
+
+                    plt.title(
+                        f"Plot of Intensity delta ({table.replace('video_', '')}) for q={q} vs frame difference tau"
+                    )
+                    plt.ylabel(f"I(q={q}, tau)")
+                    plt.xlabel("tau")
+                    plt.plot(tau, data)
+                    plt.show()
+                elif q < 0:
+                    break
+                else:
+                    print("Invalid q selected!")
+        else:
+            print("Invalid table selected!")
     else:
         print("Invalid args")
